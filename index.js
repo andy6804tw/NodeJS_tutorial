826cdda0ecbf38abf0b64983ed106742e1786e3e
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-09-25 11:50:15 +0800

    http server

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var response=require('./methods.js');
+var http=require('http');
+var server=http.createServer(engine);
 
+server.listen(1337,function(){
+    console.log("success creade!");
+})
 
-console.log(response.data.say(1,3));
+function engine(request,response){
+    response.writeHead(200,{"Content-Type":"text/plan"});
+    response.end("Hello"+request.url);
+}
